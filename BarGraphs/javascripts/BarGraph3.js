0a7a474605b3ab8e0edb8874b1ce1b97cfb5fcd3
cunnellp5 <philip.cunnell@colorado.edu>
2017-06-05 11:51:01 -0600

    added padding betwixt the bars :sparkles:

--- BarGraphs/javascripts/BarGraph3.js
+++ BarGraphs/javascripts/BarGraph3.js
@@ -8,7 +8,8 @@
 // }
 //
 // var data = RandGen()
-
+(function() {
+  'use strict';
 var width = 960;
 var height = 500;
 
@@ -45,3 +46,56 @@ function type(d) {
   d.value = +d.value;
   return d;
 }
+}());
+// *******************
+//  chart 2 code below:
+(function() {
+  'use strict';
+var width = 960,
+    height = 500;
+
+var x = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.1);
+
+var y = d3.scaleLinear()
+    .range([height, 0]);
+
+var chart = d3.select(".chart2")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.tsv("../views/abc.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.name; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + x(d.name) + ",0)"; });
+
+  bar.append("rect")
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("width", x.bandwidth());
+
+  bar.append("text")
+      .attr("x", x.bandwidth() / 2)
+      .attr("y", function(d) { return y(d.value) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.value; });
+});
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
+}());
+
+
+
+(function() {
+  'use strict';
+
+
+}());
