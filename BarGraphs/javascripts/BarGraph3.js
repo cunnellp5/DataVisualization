412bd350a96f75b2a91ee2b91d62dca38f4818de
cunnellp5 <philip.cunnell@colorado.edu>
2017-06-05 12:35:21 -0600

    graphs three

--- BarGraphs/javascripts/BarGraph3.js
+++ BarGraphs/javascripts/BarGraph3.js
@@ -47,7 +47,7 @@ function type(d) {
   return d;
 }
 }());
-// *******************
+// ****************************************************************
 //  chart 2 code below:
 (function() {
   'use strict';
@@ -92,10 +92,59 @@ function type(d) {
 }
 }());
 
-
+// part 3 ******************
 
 (function() {
   'use strict';
 
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scaleBand()
+  .rangeRound([0, width])
+  .padding(0.1)
+
+var y = d3.scaleLinear()
+  .range([height, 0]);
+
+  var xAxis = d3.axisBottom()
+      .scale(x);
+
+  var yAxis = d3.axisLeft()
+      .scale(y);
+
+var chart = d3.select('.chart3')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+d3.tsv('../views/data.tsv', type, function(error, data) {
+  x.domain(data.map(function(d) { return d.name; }))
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  chart.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
+
+  chart.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis);
+
+  chart.selectAll('.bar')
+      .data(data)
+    .enter().append('rect')
+      .attr('x', function(d) { return x(d.name); })
+      .attr('y', function(d) { return y(d.value); })
+      .attr('height', function(d) { return height - y(d.value); })
+      .attr('width', x.bandwidth());
+})
+
+function type(d) {
+  d.value = +d.value;
+  return d;
+}
 
 }());
